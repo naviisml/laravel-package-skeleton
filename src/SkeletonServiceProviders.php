9b7152174933a48a51c745c0fd6ccf8b5a7065ab
Navi Ismail <nismail@fletcher.nl>
2023-03-09 13:53:59 +0100

    :new: Added basic functionality for packages

---
+++ src/SkeletonServiceProviders.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace VendorName\Skeleton;
+
+use Naviisml\Package\Package;
+use Naviisml\Package\PackageServiceProvider;
+use VendorName\Skeleton\Commands\SkeletonCommand;
+
+class SkeletonServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/naviisml/laravel-package-skeleton
+         */
+        $package
+            ->name('skeleton')
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasMigration('create_skeleton_table')
+            ->hasCommand(SkeletonCommand::class);
+    }
+}
